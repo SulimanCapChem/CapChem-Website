556464f1cc1f5fb5631f2a1107edaa52f4ea263c
sulimansharif <sharifsuliman1@gmail.com>
2018-08-30 01:18:37 -0500

    Added all the files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Layout, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+    <Layout>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
+
+    );
+  }
+}
+
+export default App;
